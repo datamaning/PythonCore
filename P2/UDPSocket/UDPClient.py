c383519fc0db9e7a8bd2efacc2166ee41d51f099
datamaning <george_xl@163.com>
2016-10-30 18:33:36 +0800

    UDPSocket

---
+++ P2/UDPSocket/UDPClient.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+
+from socket import *
+from time import ctime
+
+HOST='localhost'
+PORT=21658
+BUFSIZ=1024
+ADDR=(HOST,PORT)
+
+udpCliSock=socket(AF_INET,SOCK_DGRAM)
+
+while True:
+    data=input('>')
+    if not  data:
+        break
+    udpCliSock.sendto(data,ADDR)
+    data,ADDR=udpCliSock.recvfrom(BUFSIZ)
+    if not  data:
+        break
+    print(data)
+udpCliSock.close()
