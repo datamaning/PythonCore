77b7a2e8e1574f5ce6f11c77ddd2d3d4e0db832c
datamaning <george_xl@163.com>
2017-01-07 09:54:59 +0800

    update now

--- P2/DATABASE/ushuffle_db.py
+++ P2/DATABASE/ushuffle_db.py
@@ -58,9 +58,66 @@ def create(cur):
         create(cur)
 drop=lambda cur:cur.execute("Drop table users")
 
-NAMES={
-        ('arron',8312) 
-        }
-        )
+NAMES=(('arron',8312),('Angela',7603),('dave',7306),('davina',7902),('elliot',7911),('ernie',7410),('jess',7912),('jim',7512),('larry',7311),('leslie',7808),('melissa',8602),('pat',7711),('serena',7003),('stan',7607),('faye',6812),('amy',7209),('mona',7404),('jennifer',7608),)
+
+def randName():
+    pick=set(NAMES)
+    while pick:
+        yield pick.pop()
+def insert(cur,db):
+    if db=='mysql':
+        cur.executemay("INSERT INTO USERS VALUES(%s,%s,%s)",[(who,uid,rand(1,5))for who,uid in randName()])
+
+def update(cur):
+    fr=rand(1,5)
+    to=rand(1,5)
+    cur.execute("UPDATE USERS SET projid=%d where projid=%d" % (to,fr))
+    return fr,to,getRC(cur)
+
+def delete(cur):
+    rm=rand(1,5)
+    cur.execute("DELETE FROM USERS WHERE projid=%d" %rm)
+    return rm,getRC(cur)
+
+def dbDUMP(cur):
+    cur.execute("SELECT * FROM USERS")
+    print '\n%s' % ''.join(map(cformat,FIELDS))
+    for data in cur.fetchall():
+        print ''.join(map(tformat,data))
+
+def main():
+    db=setup()
+    print '***Connect to %r database ' % db
+    cxn=connect(db)
+    if not cxn:
+        print 'Error: %r not supported or unreachable,existing' % db 
+        return
+    cur=cxn.cursor()
+   
+    print '\n***Create users table (drop old one if appl.)'
+    create(cur)
+    print '\n***Insert names into table'
+    insert(cur,db)
+    dbDump(cur)
+
+    print '\n***Move users to a random group'
+    fr,to,num=update(cur)
+    print '\t(%d users moved) from (%d) to (%d)' % (num,fr,to)
+    dbDump(cur)
+
+    print '\n*** Randomly delete group'
+    rm,num=delete(cur)
+    print '\t(group #%d; %d users removed)' % (rm,num)
+    dbDump(cur)
+
+    print '\n*** Drop users table'
+    drop(cur)
+    print '\n*** Close cxns'
+    cur.close()
+    cxn.commit()
+    cxn.close()
+
+if __name__=='__main__':
+    main()
 
 
