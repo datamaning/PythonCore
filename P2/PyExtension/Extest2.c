d459e7a979bd0d6169916504b71ebb4d1d5397fe
datamaning <george_xl@163.com>
2017-01-07 16:42:27 +0800

    c2p

--- P2/PyExtension/Extest2.c
+++ P2/PyExtension/Extest2.c
@@ -21,8 +21,9 @@ char *reverse(char *s)
     return s;
 }
 
-int main()
+int test()
 {
+
     char s[BUFSIZ];
     printf("4! ==%d\n",fac(4));
     printf("8! ==%d\n",fac(8));
@@ -35,4 +36,52 @@ int main()
     printf("reversing 'madam',we get '%s'\n", \
             reverse(s));
     return 0;
+}
+
+#include"Python.h"
+
+static PyObject *
+Extest_fac(PyObject *self,PyObject *args)
+{
+    int num;
+    if(!PyArg_ParseTuple(args,"i",&num))
+        return NULL;
+    return (PyObject*)Py_BuildValue("i",fac(num));
+}
+
+static PyObject *
+Extest_doppel(PyObject *self,PyObject *args)
+{
+    char *orig_str;
+    char *dupe_str;
+    PyObject* retval;
+
+    if(!PyArg_ParseTuple(args,"s",&orig_str))
+        return NULL;
+    retval=(PyObject*)Py_BuildValue("ss",orig_str, \
+            dupe_str=reverse(strdup(orig_str)));
+    free(dupe_str);
+    return retval;
+
+}
+
+static PyObject *
+Extest_test(PyObject *self,PyObject *args)
+{
+    test();
+    return (PyObject*)Py_BuildValue("");
+
+}
+
+static PyMethodDef
+ExtestMethods[]={
+    {"fac",Extest_fac,METH_VARARGS},
+    {"doppel",Extest_doppel,METH_VARARGS},
+    {"test",Extest_test,METH_VARARGS},
+    {NULL,NULL}
+};
+
+void initExtest()
+{
+    Py_InitModule("Extest",ExtestMethods);
 }
