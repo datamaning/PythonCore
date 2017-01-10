721ecd864aedcc568dd3f943183d3b1444120d47
datamaning <george_xl@163.com>
2017-01-11 00:20:43 +0800

    use mysql for django

--- P2/Web/mysite/mysite/settings.py
+++ P2/Web/mysite/mysite/settings.py
@@ -74,14 +74,24 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        #'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        'NAME':'/tmp/mysite.db',
-        }
-}
+#DATABASES = {
+ #   'default': {
+#       'ENGINE': 'django.db.backends.sqlite3',
+#       'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#       'NAME':'/tmp/mysite.db',
+#               }
+#           }
 
+DATABASES={
+        'default':{
+            'ENGINE':'django.db.backends.mysql',
+            'NAME':'TEST',
+            'USER':'root',
+            'PASSWORD':'',
+            'HOST':'',
+            'PORT':'',
+            }
+        }
 
 # Password validation
 # https://docs.djangoproject.com/en/1.10/ref/settings/#auth-password-validators
