cb58bd0419f01a337b713a10e8ab1b1c8f32ce10
datamaning <george_xl@163.com>
2017-01-10 23:53:32 +0800

    models.py

--- P2/Web/mysite/blog/models.py
+++ P2/Web/mysite/blog/models.py
@@ -2,4 +2,8 @@ from __future__ import unicode_literals
 
 from django.db import models
 
+class BlogPost(models.Model):
+    title=models.CharField(max_length=150)
+    body=models.TextField()
+    timestamp=model.DateTimeField()
 # Create your models here.
