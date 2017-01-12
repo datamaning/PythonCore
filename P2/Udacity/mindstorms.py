cecd09bc965e5fbdfb6eab60083c355d42e92cba
datamaning <george_xl@163.com>
2017-01-12 13:09:36 +0800

    new version

--- P2/Udacity/mindstorms.py
+++ P2/Udacity/mindstorms.py
@@ -13,5 +13,11 @@ def draw_square():
     brad.forward(100)
     brad.right(90)
     brad.forward(100)
+
+
+    angie=turtle.Turtle()
+    angie.shape('arrow')
+    angie.color('blue')
+    angie.circle(100)
     window.exitonclick()
 draw_square()
