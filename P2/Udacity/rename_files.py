18c70c81491b99cb04f87763c7dcfb351ad0d9e6
datamaning <george_xl@163.com>
2017-01-12 10:43:50 +0800

    new

--- P2/Udacity/rename_files.py
+++ P2/Udacity/rename_files.py
@@ -3,7 +3,10 @@ import os
 
 def rename_files():
     file_list=os.listdir(r'image/')
-    
+    print file_list    
+    saved_path=os.getcwd()
+    print("Current Working Directory is" + saved_path)
+    os.chdir(r'image')
     for file_name in file_list:
         os.rename(file_name,file_name.translate(None,'0123456789'))
 
