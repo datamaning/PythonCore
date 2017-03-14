4ed25c17332d691be61631c9c2953e808ad26d96
datamaning <george_xl@163.com>
2017-03-14 10:30:34 +0800

    url text

--- Scrapy/url_text.py
+++ Scrapy/url_text.py
@@ -3,7 +3,7 @@
 
 from lxml import etree
 import re
-from pymongo import MongoClient
+#from pymongo import MongoClient
 import socket
 import threading
 from Queue import Queue
@@ -13,7 +13,7 @@ import sys
 import traceback
 import os
 import shutil
-from html.parser import HTMLParser
+#from html.parser import HTMLParser
 import urllib
 import json
 import re
@@ -23,6 +23,7 @@ PgEr = 0
 reload(sys)
 sys.setdefaultencoding('utf-8')
 
+
 host='www.ximalaya.com' 
 albums_error = 0
 sound_error  = 0 
@@ -31,10 +32,11 @@ sound_dir='data/'
 album_url_dir='Album/data/'
 album_dir='album_test'
 url_text=os.getcwd()+'/txt/'
-def albumGetSounds(ab_url): 
+def albumGetSounds(ab_url,sound_type): 
 
     try:
         #print 'ab_url',ab_url,'os-cwd',os.getcwd()
+        
         request=urllib2.Request(ab_url)
         request.add_header('User-Agent','fake-client')
         
@@ -43,13 +45,14 @@ def albumGetSounds(ab_url):
         sound_urls = tree.xpath("//div[@class='miniPlayer3']/a/@href")
         album_title = tree.xpath("//div[@class='detailContent_title']/h1")[0].text
         album_title=re.sub('//',"",album_title)
+        cur_dir=ab_url.split('/')[5]
         #print "ab_url",ab_url
         if sound_urls:
             for sound_url in sound_urls:
                 sound_url = host + sound_url
-                print sound_url
+                #print sound_url
                 soundpage(str(ab_url), str(album_title), str(sound_url))
-            
+        os.chdir('..')    
                 
     except Exception as e:
         #print ">>> albumGetSounds error,error %s,album url is %s" % (e,ab_url)
@@ -82,25 +85,28 @@ def soundpage(a_url, a_title, s_url):
         channel_id=a_url.split('/')[3]
         album_id=a_url.split('/')[5]
         sound_id=s_url.split('/')[5]
-        print music_json['play_path']
+        #print music_json['play_path']
         #filename=channel_id+"_"+album_id+"_"+sound_id
-        url_sub=re.sub('/','',music_json['play_path'])
+        url_sub_list=music_json['play_path'].split('/')
+        url_sub=url_sub_list[3][5:]+url_sub_list[4]+url_sub_list[5]+url_sub_list[6]+url_sub_list[7].split('.')[0]
+        #url_sub=re.sub('/','',music_json['play_path'])
 
         filename=channel_id+"_"+album_id+"_"+sound_id+url_sub
-        print filename,'filename***********' 
-        sound_file=open(url_text+filename+'.txt','w+')
-        a_title='album_title='+a_title
-        title = 'sound_title='+tree.xpath("//div[@class='detailContent_title']/h1")[0].text
-        music_type ='music_type='+tree.xpath("//div[@class='detailContent_category']/a")[0].text
+        print os.getcwd(),filename
+        #print filename,'filename***********' 
+        sound_file=open(filename+'.txt','w+')
+        a_title='album_title = '+a_title
+        title = 'sound_title = '+tree.xpath("//div[@class='detailContent_title']/h1")[0].text
+        music_type ='music_type = '+tree.xpath("//div[@class='detailContent_category']/a")[0].text
         tags = tree.xpath("//div[@class='tagBtnList']/a[@class='tagBtn2']/span")
-        tagString = 'tagString='+','.join(i.text for i in tags)
-        playcount = 'playcount='+tree.xpath("//div[@class='soundContent_playcount']")[0].text
-        likecount = 'likecount='+tree.xpath("//a[@class='likeBtn link1 ']/span[@class='count']")[0].text
-        commentcount = 'commentcount='+tree.xpath("//a[@class='commentBtn link1']/span[@class='count']")[0].text
-        forwardcount = 'forwardcount='+tree.xpath("//a[@class='forwardBtn link1']/span[@class='count']")[0].text
-        mp3duration = 'md3duration='+tree.xpath("//div[@class='sound_titlebar']/div[@class='fr']/span[@class='sound_duration']")[0].text    
+        tagString = 'tagString = '+','.join(i.text for i in tags)
+        playcount = 'playcount = '+tree.xpath("//div[@class='soundContent_playcount']")[0].text
+        likecount = 'likecount = '+tree.xpath("//a[@class='likeBtn link1 ']/span[@class='count']")[0].text
+        commentcount = 'commentcount = '+tree.xpath("//a[@class='commentBtn link1']/span[@class='count']")[0].text
+        forwardcount = 'forwardcount = '+tree.xpath("//a[@class='forwardBtn link1']/span[@class='count']")[0].text
+        mp3duration = 'md3duration = '+tree.xpath("//div[@class='sound_titlebar']/div[@class='fr']/span[@class='sound_duration']")[0].text    
         username = tree.xpath("//div[@class='username']")[0].text
-        username = 'username='+username.split()[0]
+        username = 'username = '+username.split()[0]
 
         sound_file.write(str(a_title+'\n'))
         sound_file.write(str(title+'\n'))
@@ -111,9 +117,9 @@ def soundpage(a_url, a_title, s_url):
         sound_file.write(str(commentcount+'\n'))
         sound_file.write(str(forwardcount+'\n'))
         sound_file.write(str(mp3duration+'\n'))
-        sound_file.write(str("albumurl="+a_url+'\n'))
-        sound_file.write(str('soundurl='+s_url+'\n'))
-        sound_file.write(str('downloadurl='+download_url)+'\n')
+        sound_file.write(str("albumurl = "+a_url+'\n'))
+        sound_file.write(str('soundurl = '+s_url+'\n'))
+        sound_file.write(str('downloadurl = '+download_url)+'\n')
         sound_file.write(str(username+'\n'))
         
         sound_file.close()
@@ -154,22 +160,26 @@ if __name__ == '__main__':
         album_title=[]
         #按声音类型建立文件夹
         f=open(filename,'r')
-        '''
-        ff=filename.split('.')[0]
-        if not os.path.exists(ff):
-            os.mkdir(ff)
-        os.chdir(ff)
-        '''
+        
+        #ff=filename.split('.')[0]
+        os.chdir(url_text)
+        index=len(filename.split('/'))-1
+        sound_type=filename.split('/')[index].split('.')[0]
+        if not os.path.exists(sound_type):
+            os.mkdir(sound_type)
+        os.chdir(sound_type)
+        #print os.getcwd(),'os cwd',filename
+        
         for line in f:
             l=line.strip()
             l=line.split(',')
             album_url.append(l[2])
             album_file_name=l[0]+"_"+l[1]
             album_title.append(album_file_name)
+
             #album_dir=os.getcwd()+'/'+album_file_name 
             res=l[3].decode('utf-8')
             r=re.sub(r"/",'',res)
-            #print os.getcwd()
             albums_error=0
             sound_error=0
             
@@ -185,10 +195,16 @@ if __name__ == '__main__':
                 max_page=1
             else:
                 max_page = max(pages)
-            print max_page
+            #print max_page
             for page in xrange(1, max_page+1):    
-                albumGetSounds(l[2]+'?page='+str(page))
+                os.chdir(txt_dir+'/'+sound_type)
+                album_id=a_url.split('/')[5].split('?')[0]
+                if not os.path.exists(album_id):
+                    os.mkdir(album_id)
+                os.chdir(album_id)
+                albumGetSounds(l[2]+'?page='+str(page),sound_type)
 
+                
             #if not os.path.exists(r):               
                # os.mkdir(r)
                 #print l[3].decode('utf-8').strip(), os.getcwd()
