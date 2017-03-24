0a80f3bdeeb56d14c60a78db6aa705be677c3d36
datamaning <george_xl@163.com>
2017-03-24 13:34:58 +0800

    add

--- P2/algorithm/bayes.py
+++ P2/algorithm/bayes.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # encoding: utf-8
-
+from numpy import *
 def loadDataSet():
     postingList=[['my','dog','has','flea','problems','help','please'],\
             ['maybe','not','take','him','to','dog','park','stupid'],
@@ -18,7 +18,7 @@ def createVocabList(dataSet):
         vocabSet=vocabSet|set(document)
     return list(vocabSet)
 
-def sefOfWordsVec(vocabList,inputSet):
+def setOfWordsVec(vocabList,inputSet):
     returnVec=[0]*len(vocabList)
     for word in inputSet:
         if word in vocabList:
@@ -26,3 +26,37 @@ def sefOfWordsVec(vocabList,inputSet):
         else:
             print "the word : %s is not in my Vocabulary!" %word
     return returnVec
+listPosts,listClasses=loadDataSet()
+myVocabList=createVocabList(listPosts)
+print myVocabList
+print setOfWordsVec(myVocabList,listPosts[0])
+trainMat=[]
+for postinDoc in listPosts:
+    trainMat.append(setOfWordsVec(myVocabList,postinDoc))
+print trainMat
+
+
+def trainNB0(trainMatrix,trainCategory):
+    numTrainDocs=len(trainMatrix)
+    print numTrainDocs
+    numWords=len(trainMatrix[0])
+    print numWords
+    pAbusive=sum(trainCategory)/float(numTrainDocs)
+    print pAbusive
+
+    p0Num=zeros(numWords)
+    p1Num=zeros(numWords)
+    print p0Num,len(p0Num)
+    p1Denom=0.0;p0Denom=0.0
+    for i in range(numTrainDocs):
+        if trainCategory[i]==1:
+            p1Num+=trainMatrix[i]
+            p1Denom+=sum(trainMatrix[i])
+        else:
+            p0Num+=trainMatrix[i]
+            p0Denom+=sum(trainMatrix[i])
+    p1Vect=p1Num/p1Denom
+    p0Vect=p0Num/p0Denom
+    print  p0Vect,p1Vect
+    return p0Vect,p1Vect,pAbusive
+trainNB0(trainMat,listClasses)
