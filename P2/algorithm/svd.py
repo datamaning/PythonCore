cefa16cbff0d26755ec8c003bc4f625f855d4264
datamaning <george_xl@163.com>
2017-03-28 14:34:57 +0800

    modify svd

--- P2/algorithm/svd.py
+++ P2/algorithm/svd.py
@@ -20,7 +20,8 @@ print 'VT',VT
 from numpy import linalg as la
 
 def ecludSim(inA,inB):
-    return 1.0*(1.0+la.norm(inA-inB))
+    #print la.norm(inA-inB)
+    return 1.0/(1.0+la.norm(inA-inB))
 
 def pearsSim(inA,inB):
     if len(inA) <3 :return 1.0
@@ -29,9 +30,54 @@ def pearsSim(inA,inB):
 def cosSim(inA,inB):
     num=float(inA.T*inB)
     denom=la.norm(inA)*la.norm(inB)
-    return 0,5+0.5*(num/denom)
+    return 0.5+0.5*(num/denom)
 
 myMat=mat(data)
+'''
 print ecludSim(myMat[:,0],myMat[:,4])
-
 print ecludSim(myMat[:,0],myMat[:,0])
+
+print cosSim(myMat[:,0],myMat[:,4])
+print cosSim(myMat[:,0],myMat[:,0])
+'''
+
+def standEst(dataMat,user,simMeas,item):
+    n=shape(dataMat)[1]
+    simTotal=0.0
+    ratSimTotal=0.0
+    for j in range(n):
+        userRating=dataMat[user,j]
+        if userRating ==0: continue
+        overLap=nonzero(logical_and(dataMat[:,item].A>0,
+            dataMat[:,j].A>0))[0]
+        if len(overLap)==0:similarity=0
+        else:similarity=simMeas(dataMat[overLap,item],dataMat[overLap,j])
+        #print similarity,simTotal,overLap
+        simTotal+=similarity
+
+        ratSimTotal+=similarity*userRating
+    if simTotal==0:return 0
+    else: return ratSimTotal/simTotal
+
+def recommend(dataMat,user,N=3,simMeas=cosSim,estMethod=standEst):
+    unratedItems=nonzero(dataMat[user,:].A==0)[1]
+    #print type(unratedItems),len(unratedItems),unratedItems
+    if len(unratedItems)==0: return 'you rated everything'
+    itemScores=[]
+    for item in unratedItems:
+        estimatedScore=estMethod(dataMat,user,simMeas,item)
+        itemScores.append((item,estimatedScore))
+    return sorted(itemScores,key=lambda jj:jj[1],reverse=True)[:N]
+
+myMat[0,1]=myMat[0,0]=myMat[1,0]=myMat[2,0]=4
+myMat[3,3]=2
+myMat=mat([[4,4,0,2,2],
+           [4,0,0,3,3],
+           [4,0,0,1,1],
+           [1,1,1,2,0],
+           [2,2,2,0,0],
+           [1,1,1,0,0],
+           [5,5,5,0,0]
+    ])
+print myMat[[0,3,4,5,6],2]
+print recommend(myMat,2)
